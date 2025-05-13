28c72e44dd4522f5a5a69e71230ceb1dbece6478
NadirAli17 <nadirali81835@gmail.com>
2025-05-12 17:58:04 -0700

    First push, base tempelate code

---
+++ Unlmtd/Unlmtd/UnlmtdApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnlmtdApp.swift
+//  Unlmtd
+//
+//  Created by Nadir Ali on 5/12/25.
+//
+
+import SwiftUI
+
+@main
+struct UnlmtdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Unlmtd/UnlmtdTests/UnlmtdTests.swift
@@ -0,0 +1,17 @@
+//
+//  UnlmtdTests.swift
+//  UnlmtdTests
+//
+//  Created by Nadir Ali on 5/12/25.
+//
+
+import Testing
+@testable import Unlmtd
+
+struct UnlmtdTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
